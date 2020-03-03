666e0a542f491a7e07374a2d527c68013651ffce
Denis <60355197+Denisblch@users.noreply.github.com>
2020-03-03 15:31:26 +0300

    wrote methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,61 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
 
     void clear() {
+        for(int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) storage[i] = null;
+            else break;
+        }
     }
 
     void save(Resume r) {
+        for(int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for(Resume i: storage) {
+            if (i != null && i.uuid.equals(uuid))
+                return i;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int count = 0;
+        for(int i = 0; i < storage.length; i++) {
+            if(storage[i] != null && storage[i].uuid.equals(uuid)) {
+                count = i;
+                break;
+            }
+        }
+        for(int i = count; i < storage.length-1; i++) {
+            storage[i] = storage[i + 1];
+            storage[i + 1] = null;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] news = new Resume[size()];
+        if (news.length >= 0) System.arraycopy(storage, 0, news, 0, news.length);
+        return news;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for(Resume i :storage) {
+            if (i != null) count++;
+            else break;
+        }
+        return count;
     }
 }
