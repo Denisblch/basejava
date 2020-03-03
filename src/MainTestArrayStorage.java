836f7e1860f03ec8679fd4cd9e5fea0551dbc87c
Denis <60355197+Denisblch@users.noreply.github.com>
2020-03-03 15:36:59 +0300

    Add files via upload

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -2,7 +2,7 @@
  * Test for your ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    private static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
@@ -18,7 +18,6 @@ public class MainTestArrayStorage {
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
         System.out.println("Size: " + ARRAY_STORAGE.size());
-
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
@@ -30,7 +29,7 @@ public class MainTestArrayStorage {
         System.out.println("Size: " + ARRAY_STORAGE.size());
     }
 
-    static void printAll() {
+    private static void printAll() {
         System.out.println("\nGet All");
         for (Resume r : ARRAY_STORAGE.getAll()) {
             System.out.println(r);
