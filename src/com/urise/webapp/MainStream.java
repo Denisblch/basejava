de09166913b44a487901e47835c4c344dbb6ab0c
Denis <60355197+Denisblch@users.noreply.github.com>
2020-08-18 21:03:59 +0300

    HW_12. MainStream

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -8,7 +8,7 @@ public class MainStream {
 
     public static void main(String[] args) {
         List<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-        int[] values = new int[]{9, 9, 8, 9, 9, 1, 3};
+        int[] values = new int[]{9, 9, 8, 9, 9, 1, 9};
         System.out.println(minValue(values));
         System.out.println(oddOrEven(array));
     }
@@ -18,14 +18,7 @@ public class MainStream {
      */
     public static int minValue(int[] values) {
         IntStream streamFromArrays = Arrays.stream(values);
-        int[] mas = streamFromArrays.sorted().distinct().toArray();
-        int retval = (int) Math.pow(10, mas.length - 1);
-        int sum = 0;
-        for (int digit : mas) {
-            sum += (retval * digit);
-            retval /= 10;
-        }
-        return sum;
+        return streamFromArrays.sorted().distinct().reduce(0, (x, y) -> (10 * x) + y);
     }
 
     /**
@@ -33,28 +26,12 @@ public class MainStream {
      */
     public static List<Integer> oddOrEven(List<Integer> integers) {
         int count = integers.stream().mapToInt((Int) -> Integer.parseInt(String.valueOf(Int))).sum();
-        if (count % 2 == 0) {
-            return integers.stream().filter(x -> x % 2 != 0).collect(Collectors.toList());
-        } else {
-            return integers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
-        }
+        return integers.stream().filter(x -> {
+            if (count % 2 == 0) {
+                return x % 2 != 0;
+            } else {
+                return x % 2 == 0;
+            }
+        }).collect(Collectors.toList());
     }
-
-   /* public static void processingOfList(List<Integer> integers, int count) {
-        if (count % 2 == 0) {
-            integers.stream().filter(x -> x % 2 == 0).;
-            for (int i = 0; i < integers.size(); i++) {
-                if ((integers.get(i) % 2) == 0) {
-                    integers.remove(integers.get(i));
-                }
-            }
-        } else {
-            integers.stream().filter(x -> x % 2 != 0);
-            for (int i = 0; i < integers.size(); i++) {
-                if ((integers.get(i) % 2) != 0) {
-                    integers.remove(integers.get(i));
-                }
-            }
-        }
-    }*/
 }
