d2b19743baa67d23b091980d9c80e8f9321150b4
Denis <60355197+Denisblch@users.noreply.github.com>
2020-08-27 19:58:22 +0300

    HW_14. SqlTransaction

--- src/com/urise/webapp/sql/SqlHelper.java
+++ src/com/urise/webapp/sql/SqlHelper.java
@@ -1,6 +1,5 @@
 package com.urise.webapp.sql;
 
-import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.StorageException;
 
 import java.sql.Connection;
@@ -23,9 +22,22 @@ public class SqlHelper {
              PreparedStatement ps = connection.prepareStatement(string)) {
             return se.execution(ps);
         } catch (SQLException e) {
-            if (e.getSQLState().equals("23505")) {
-                throw new ExistStorageException(e);
+            throw ExceptionUtil.convertException(e);
+        }
+    }
+
+    public <T> T transactionalExecute(SqlTransaction<T> executor) {
+        try (Connection conn = connectionFactory.getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+                T res = executor.execute(conn);
+                conn.commit();
+                return res;
+            } catch (SQLException e) {
+                conn.rollback();
+                throw ExceptionUtil.convertException(e);
             }
+        } catch (SQLException e) {
             throw new StorageException(e);
         }
     }
